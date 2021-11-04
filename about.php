d5e7b0c319147bf4b7118defd9eb10923288ce26
Frazer336 <70014726+Frazer336@users.noreply.github.com>
2021-11-05 11:36:06 +1300

    Add content to other pages
    added calendar and info on about

--- about.php
+++ about.php
@@ -11,6 +11,104 @@
 
 
 
+<!-- ======= Pricing Section ======= -->
+<section id="pricing" class="pricing">
+      <div class="container">
+
+        <div class="section-title">
+          <h1>Southland Clubs</h1>
+          <br>
+          <h2>Clubs</h2>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6">
+            <div class="box">
+              <h3>Masters Badminton Club</h3>
+              <ul>
+                <li>$30 Initial Subscription Fee</li>
+                <li>Age Group: +35</li> 
+              </ul>
+              <div class="btn-wrap">
+                <a href="https://www.facebook.com/SouthlandMasters/" class="btn-buy" target="_blank">Find Out More</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mt-4 mt-md-0">
+            <div class="box">
+              <h3>All Saints Badminton Club</h3>
+              <ul>
+                <li>Buy 10 nights now, get the next night free</li>
+                <li>Age Group: All Welcome</li>
+              </ul>
+              <div class="btn-wrap">
+                <a href="https://allsaintssouthland.wixsite.com/badminton" class="btn-buy" target="_blank">Find Out More</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mt-4 mt-lg-0">
+            <div class="box">
+              <h3>Smash Badminton</h3>
+              <ul>
+                <li>$15 Initial Subscription Fee</li>
+                <li>Age Group: All Welcome</li>
+              </ul>
+              <div class="btn-wrap">
+                <a href="https://www.facebook.com/pages/category/Sports-Club/Smash-Badminton-Club-Invercargill-NZ-108955022642539/" class="btn-buy" target="_blank">Find Out More</a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Pricing Section -->
+
+
+    <!-- ======= sponsor Section ======= -->
+    <section id="sponsor" class="sponsor section-bg">
+      <div class="container">
+        <div class="section-title">
+            <h2>Sponsors</h2>
+            <h3>Thank You To All Of Our <span>Sponsors</span></h3>
+          </div>
+
+        <div class="row" data-aos="zoom-in">
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/sponsors/ilt_f.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/sponsors/mcculloch.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/sponsors/mitre10.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/sponsors/south_port.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/sponsors/community_south.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/sponsors/southern_trust.png" class="img-fluid" alt="">
+          </div>
+
+
+
+        </div>
+
+      </div>
+    </section><!-- End sponsor Section -->
+    
 
 <!-- Footer -->
 <?php include 'footer.php'; ?>
--- calendar.php
+++ calendar.php
@@ -9,7 +9,175 @@
 <!-- header -->
 <?php include 'header.php'; ?>
 
- 
+        <!-- ======= calendar Section ======= -->
+        <section id="calendar" class="section-bg" style="margin-top: 100px;">
+            <div class="container" data-aos="fade-up">
+      
+              <header class="section-header">
+                <h3 class="section-title">2021 Calendar</h3>
+              </header>
+      
+              <div class="row" data-aos="fade-up" data-aos-delay="100"">
+            <div class=" col-lg-12">
+                <ul id="calendar-flters">
+                  <li data-filter="*" class="filter-active">All</li>
+                  <li data-filter=".filter-u13">U13 & U15</li>
+                  <li data-filter=".filter-u17">U17 & U19</li>
+                  <li data-filter=".filter-seniors">Seniors</li>
+                  <li data-filter=".filter-masters">Masters</li>
+                </ul>
+              </div>
+            </div>
+      
+            <div class="row calendar-container" data-aos="fade-up" data-aos-delay="200">
+      
+              <div class="col-lg-4 col-md-6 calendar-item filter-seniors">
+                <div class="calendar-wrap">
+                  <figure>
+                    <img src="assets/img/calendar/nats_2.JPG" class="img-fluid" alt="">
+                  </figure>
+      
+                  <div class="calendar-info">
+                    <h4>Southland Open</h4>
+                    <i class='bx bx-calendar'></i>21/22 May
+                    <br>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-lg-4 col-md-6 calendar-item filter-u13">
+                <div class="calendar-wrap">
+                  <figure>
+                    <img src="assets/img/dupree.jpg" class="img-fluid" alt="">
+                  </figure>
+      
+                  <div class="calendar-info">
+                    <h4>Southland U13</h4>
+                    <i class='bx bx-calendar'>21/22 May</i>
+                    <br>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-lg-4 col-md-6 calendar-item filter-u13">
+                <div class="calendar-wrap">
+                  <figure>
+                    <img src="assets/img/calendar/blake.JPG" class="img-fluid" alt="">
+                  </figure>
+      
+                  <div class="calendar-info">
+                    <h4>Southland U15</h4>
+                    <i class='bx bx-calendar'>21/22 May</i>
+                    <br>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-lg-4 col-md-6 calendar-item filter-u17">
+                <div class="calendar-wrap">
+                  <figure>
+                    <img src="assets/img/calendar/goat_brn.jpg" class="img-fluid" alt="">
+                  </figure>
+      
+                  <div class="calendar-info">
+                    <h4>Southland U17</h4>
+                    <i class='bx bx-calendar'>21/22 May</i>
+                    <br>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-lg-4 col-md-6 calendar-item filter-u17">
+                <div class="calendar-wrap">
+                  <figure>
+                    <img src="assets/img/calendar/si_1.JPG" class="img-fluid" alt="">
+                  </figure>
+      
+                  <div class="calendar-info">
+                    <h4>Southland U19</h4>
+                    <i class='bx bx-calendar'>21/22 May</i>
+                    <br>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-lg-4 col-md-6 calendar-item filter-masters">
+                <div class="calendar-wrap">
+                  <figure>
+                    <img src="assets/img/dupree.jpg" class="img-fluid" alt="">
+                  </figure>
+      
+                  <div class="calendar-info">
+                    <h4>Southland Masters</h4>
+                    <i class='bx bx-calendar'>21/22 May</i>
+                    <br>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-lg-4 col-md-6 calendar-item filter-seniors">
+                <div class="calendar-wrap">
+                  <figure>
+                    <img src="assets/img/calendar/jb_1.JPG" class="img-fluid" alt="">
+                  </figure>
+      
+                  <div class="calendar-info">
+                    <h4>South Island Open</h4>
+                    <i class='bx bx-calendar'>21/22 May</i>
+                    <br>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-lg-4 col-md-6 calendar-item filter-u13 filter-u17">
+                <div class="calendar-wrap">
+                  <figure>
+                    <img src="assets/img/calendar/team_2.JPG" class="img-fluid" alt="">
+                  </figure>
+      
+                  <div class="calendar-info">
+                    <h4>South Island</h4> 
+                    <h4>Junior Team Event</h4>
+                    <i class='bx bx-calendar'>21/22 May</i>
+                    <br>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-lg-4 col-md-6 calendar-item filter-u13 filter-u17">
+                <div class="calendar-wrap">
+                  <figure>
+                    <img src="assets/img/calendar/si_1.jpg" class="img-fluid" alt="">
+                  </figure>
+      
+                  <div class="calendar-info">
+                    <h4>South Island</h4>
+                    <h4>Junior Individual Event</h4>
+                    <i class='bx bx-calendar'>21/22 May</i>
+                    <br>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-lg-4 col-md-6 calendar-item filter-u17">
+                <div class="calendar-wrap">
+                  <figure>
+                    <img src="assets/img/calendar/jhc_2020.jpg" class="img-fluid" alt="">
+                  </figure>
+      
+                  <div class="calendar-info">
+                    <h4>Southland Secondary Schools</h4>
+                    <h4>Championships</h4>
+                    <i class='bx bx-calendar'>21/22 May</i>
+                    <br>
+                  </div>
+                </div>
+              </div>
+      
+            </div>
+      
+            </div>
+          </section><!-- End calendar Section -->
 
 <!-- Footer -->
 <?php include 'footer.php'; ?>
